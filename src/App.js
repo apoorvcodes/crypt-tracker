98d1c2b33d8a6eefff02fd4986027ac7322f6cab
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-05-05 17:28:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {axios} from "axios"
+const s = axios.get("s").then()
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div className="hm">
+<h1>Hmm</h1>
+  </div>
+  <h1>
+    ss
+  </h1>
+    </>
   );
 }
 
 export default App;
+
+/*C:\Users\totu\OneDrive\Desktop\Projects\Personal\CryptoJS**/
