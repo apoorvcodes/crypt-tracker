e5e8e50a4b90770a4e8c2c74bd54d38472ce6738
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-05-10 18:42:51 +0530

    under-work

--- src/Components/core/navbar/navbar.jsx
+++ src/Components/core/navbar/navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import {NavLink} from "react-router-dom"
 function navbar() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <nav className="navbar">
+       <div className="nav-container">
+         <NavLink exact to="/" className="nav-logo">
+           Crypto 👛
+         </NavLink>
+       </div>
+       </nav> 
+     
+    </>
   )
 }
 
