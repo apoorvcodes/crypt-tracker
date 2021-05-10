33dfe77027e93a5b749f1ac6e60eadcc1eef4afa
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-05-10 18:19:36 +0530

    dom added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from "./Components/core/navbar/navbar"
+import {BrowserRouter as Router,Switch,Route } from "react-router-dom";
 function App() {
  
   return (
