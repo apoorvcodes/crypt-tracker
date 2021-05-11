633ab6293ea73e3ce6626ea747b31a76cb52658c
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-05-11 18:48:06 +0530

    s

--- src/Components/pages/apidocs.jsx
+++ src/Components/pages/apidocs.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const apidocs = () => {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
--- src/Components/pages/contact.jsx
+++ src/Components/pages/contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const contact = () => {
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
