58e9ee645b4bf53d1b2252c403e6f749c2d1c2a9
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-05-11 18:57:38 +0530

    react is fun

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import NavBar from "./Components/core/navbar/navbar"
 import {BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import { home } from './Components/pages/home';
+import { crypto } from './Components/pages/crypto';
+import { apidocs } from './Components/pages/apidocs';
+import { github } from './Components/pages/github';
+import { contact } from './Components/pages/contact';
 function App() {
  
   return (
    <>
    <Router>
    <NavBar/>
+   <Switch>
+     <Route exact path ="/" component={home}/>
+     <Route  path ="/crypto" component={crypto}/>
+     <Route  path ="/apidocs" component={apidocs}/>
+     <Route  path ="/github" component={github}/>
+     <Route  path ="/contact" component={contact}/>
+   </Switch>
   </Router>
 
    </>
